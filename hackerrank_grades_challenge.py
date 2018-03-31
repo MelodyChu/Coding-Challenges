b07e745e2a998be18f6ef722f0f2cb132933c5ba
Melody Chu <melodychu@macbook-pro-31.lan>
2018-03-31 16:39:21 -0700

    Hackerrank grades challenge question

--- hackerrank_grades_challenge.py
+++ hackerrank_grades_challenge.py
@@ -1,4 +1,32 @@
+# def gradingStudents(grades):
+    
+#     new_grades = []
+    
+#     for i in range(1,len(grades)):
+#         if grades[i] < 38:
+#             new_grades.append(grades[i])
+#         else: # 43 should go to 45
+#             if grades[i] % 5 == 0:
+#                 new_grades.append(grades[i])
+#             elif (5*(int(grades[i] / 5) + 1)) - grades[i] < 3:
+#                 new_grades.append(5*(int(grades[i]/5) + 1))
+#             else:
+#                 new_grades.append(grades[i])
+                
+#     for grade in new_grades:
+#         print (grade)
+
+
+
+# print gradingStudents([4, 73, 67, 38, 33])
+
+from __future__ import print_function
+
+import os
+import sys
+
 def gradingStudents(grades):
+
     
     new_grades = []
     
@@ -15,7 +43,22 @@ def gradingStudents(grades):
                 
     for grade in new_grades:
         print (grade)
+    
 
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
 
+    n = int(raw_input())
 
-print gradingStudents([4, 73, 67, 38, 33])
+    grades = []
+
+    for _ in xrange(n):
+        grades_item = int(raw_input())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    f.write('\n'.join(map(str, result)))
+    f.write('\n')
+
+    f.close()
